6262c1c0080af0da43208edac31130fdd08db05d
Natan <Th3than@gmail.com>
2023-07-24 16:00:56 +0700

    lab10

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,16 @@
 "use client";
 
+import { UserCard } from "@/components/UserCard";
+import { cleanUser } from "@/libs/cleanUser";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function RandomUserPage() {
   //user = null or array of object
   const [users, setUsers] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [genAmount, setGenAmount] = useState(1);
+  const [first, setfirst] = useState(true);
 
   const generateBtnOnClick = async () => {
     setIsLoading(true);
@@ -16,11 +19,28 @@ export default function RandomUserPage() {
     );
     setIsLoading(false);
     const users = resp.data.results;
+    const cleaneduser = users.map((person) => cleanUser(person));
+    setUsers(cleaneduser);
     //Your code here
     //Process result from api response with map function. Tips use function from /src/libs/cleanUser
     //Then update state with function : setUsers(...)
   };
-
+  useEffect(() => {
+    if (first) {
+      setfirst(false);
+      return;
+    }
+    localStorage.setItem("num", genAmount);
+  }, [genAmount]);
+  useEffect(() => {
+    const getnum = localStorage.getItem("num");
+    if (getnum == null) {
+      localStorage.setItem("num", 1);
+      setGenAmount(1);
+      return;
+    }
+    setGenAmount(getnum);
+  }, []);
   return (
     <div style={{ maxWidth: "700px" }} className="mx-auto">
       <p className="display-4 text-center fst-italic m-4">Users Generator</p>
@@ -40,7 +60,17 @@ export default function RandomUserPage() {
       {isLoading && (
         <p className="display-6 text-center fst-italic my-4">Loading ...</p>
       )}
-      {users && !isLoading && users.map(/*code map rendering UserCard here */)}
+      {users &&
+        !isLoading &&
+        users.map((person) => (
+          <UserCard
+            key={person.email}
+            name={person.name}
+            imgUrl={person.imgUrl}
+            email={person.email}
+            address={person.address}
+          ></UserCard>
+        ))}
     </div>
   );
 }
